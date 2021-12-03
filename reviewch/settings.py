fa3b2d63b81b1faa632f0c77545a9d5a532d964b
Artem <artyvic@yandex.ru>
2021-12-04 01:36:05 +0300

    Fixes

--- reviewch/settings.py
+++ reviewch/settings.py
@@ -24,10 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-=1hoi%508+_mj3ulbt(7(%^-+!&9l)3*n@+*h-9=0j6*2k7*@g'
 
-PRODUCTION = False
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
